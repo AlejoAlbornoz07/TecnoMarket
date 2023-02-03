e4ba3504e05dfbaec202599b8a83cf7dd20e236b
AlejoAlbornoz07 <alejo_leon90@hotmail.com>
2023-02-02 22:01:50 -0500

    Aqui realice Jquery Validate

--- js/formulario_jquery.js
+++ js/formulario_jquery.js
@@ -1,10 +1,47 @@
 
+$.validator.addMethod("terminaPor", function(value, element, parametro){
+
+    if(value.endsWith(parametro)){
+        return true;
+    }
+
+    return false;
+
+}, "Debe terminar por {0}")
+
+$("#formulario_contacto").validate({
+    rules: {
+        nombre:{
+            required: true,
+            minlength: 3,
+            maxlentgh: 30
+        },
+        email:{
+            required: true,
+            email: true,
+            terminaPor: "duoc.cl"
+        },
+        tipo_solicitud: {
+            required: true
+        },
+        mensaje: {
+            required: true,
+            minlength: 5,
+            maxlentgh: 200
+        }
+    }
+})
+
+
 $("#guardar").click(function(){
+    if($("#formulario_contacto").valid()==false){
+        return;
+    }
     let nombre = $("#nombre").val()
     let email = $("#email").val()
     let tipoSolicitud = $("#tipo_solicitud").val()
     let mensaje = $("#mensaje").val()
     let avisos = $("#avisos").is(":checked")
 
-    console.log(nombre)
+    //
 })
