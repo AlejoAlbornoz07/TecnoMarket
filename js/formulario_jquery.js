231a26cde69bca6824ad701ff8191f41bf369327
AlejoAlbornoz07 <alejo_leon90@hotmail.com>
2023-02-02 20:14:10 -0500

    Aqui realice Jquery Validate actu

---
+++ js/formulario_jquery.js
@@ -0,0 +1,10 @@
+
+$("#guardar").click(function(){
+    let nombre = $("#nombre").val()
+    let email = $("#email").val()
+    let tipoSolicitud = $("#tipo_solicitud").val()
+    let mensaje = $("#mensaje").val()
+    let avisos = $("#avisos").is(":checked")
+
+    console.log(nombre)
+})
