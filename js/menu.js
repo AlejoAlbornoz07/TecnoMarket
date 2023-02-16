4c4da3ad1bf0dfb9fc8a088b5298b5a25e2229c3
AlejoAlbornoz07 <alejo_leon90@hotmail.com>
2023-02-16 11:30:17 -0500

    comentarios del javascript del menu hamburguesa

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,12 @@
 
 //en javascript llamamos a los elementos por el id
-let menu = document.getElementById("menu")
+let menu = document.getElementById("menu")/*con let 'nombre' hacemos una refeerencia al menu*/
 let header = document.getElementById("header")
 let nav = document.getElementById("nav")
 
-menu.addEventListener("click", function(){
+menu.addEventListener("click", function(){/*con esto evento de click y que haga una funcion*/
 
-    if(header.style.height == "90px" || header.offsetHeight == 90){
+    if(header.style.height == "90px" || header.offsetHeight == 90){/**/
         header.style.height = 90 + nav.offsetHeight + "px"
     } else{
         header.style.height = "90px"
