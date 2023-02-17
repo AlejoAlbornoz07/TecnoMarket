ef8e616b41ded4a27cef856483642b62eec1e023
AlejoAlbornoz07 <alejo_leon90@hotmail.com>
2023-02-16 21:38:10 -0500

    hasta el Jquery validator todo ok

--- js/formulario_jquery.js
+++ js/formulario_jquery.js
@@ -1,4 +1,6 @@
 
+/*funcion personalizada*/
+
 $.validator.addMethod("terminaPor", function(value, element, parametro){
 
     if(value.endsWith(parametro)){
@@ -7,34 +9,36 @@ $.validator.addMethod("terminaPor", function(value, element, parametro){
 
     return false;
 
-}, "Debe terminar por {0}")
+}, "Debe terminar por {0}")//{0} seria el parametro
 
-$("#formulario_contacto").validate({
-    rules: {
+//termina funcion personalizada
+
+$("#formulario_contacto").validate({//llamamos al formualrio, .validate para validar los datos con el json
+    rules: {//aqui las reglas
         nombre:{
             required: true,
             minlength: 3,
-            maxlentgh: 30
+            maxlength: 30
         },
         email:{
             required: true,
             email: true,
-            terminaPor: "duoc.cl"
+            terminaPor: "hotmail.com"//este seria el parametro de la "funcion personalizada"
         },
-        tipo_solicitud: {
+        tipoSolicitud:{
             required: true
         },
-        mensaje: {
+        mensaje:{
             required: true,
             minlength: 5,
-            maxlentgh: 200
+            maxlength: 200
         }
     }
 })
 
 
-$("#guardar").click(function(){
-    if($("#formulario_contacto").valid()==false){
+$("#guardar").click(function(){//Para llamar el boton por id, y hacer una funcion si se hace un click en el
+    if($("#formulario_contacto").valid()==false){//aqui llamamos al formulario por su id, para que cuando se le de al boton guardan salgan los mensajes de las validaciones, si no es valido que retorne
         return;
     }
     let nombre = $("#nombre").val()
@@ -42,6 +46,4 @@ $("#guardar").click(function(){
     let tipoSolicitud = $("#tipo_solicitud").val()
     let mensaje = $("#mensaje").val()
     let avisos = $("#avisos").is(":checked")
-
-    //
 })
--- js/menu.js
+++ js/menu.js
@@ -6,13 +6,14 @@ let nav = document.getElementById("nav")
 
 menu.addEventListener("click", function(){/*con esto evento de click y que haga una funcion*/
 
-    if(header.style.height == "90px" || header.offsetHeight == 90){/**/
-        header.style.height = 90 + nav.offsetHeight + "px"
+    if(header.style.height == "90px" || header.offsetHeight == 90){/*ahora teniendo lo de la linea siguiente, entonces si la altura del header es 90 entonces que haga lo siguiente, sino que lo deje en 90px*/
+                                      /*despues del O es porque luego de recarga la pagina se pierde la animacion porque queda sumada y acumulada la altura entonces ya no es 90px, por ello se pone el header.offsetHeight == 90*/  
+        header.style.height = 90 + nav.offsetHeight + "px"/*cuando se haga click el header crecera, pero no se utiliza auto porque no permite hacer una animacion, entonces se utiliza un calculo en este caso es 90+nav.offserHeight que es el alto del display actual mas la palabra px*/
     } else{
         header.style.height = "90px"
     }    
 })
-
+/*Hay un bug cuando intento pasar ahora al modo ventana, que el header queda muy grande, entonces se hace lo siguiente*/
 window.addEventListener("resize", function(){
 
     let ancho = document.documentElement.clientWidth
